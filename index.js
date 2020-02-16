5e66c56f2f79f09cb37b82630ee880a2803630f8
Jin Wu <jinn.wuu@gmail.com>
2020-02-16 18:27:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your solution in this file!
 
 var customerName = 'bob';
-var bestCustomer = 'not bob';
+
 
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
 }
 
 function setBestCustomer() {
-  
+  var bestCustomer = 'not bob';
 }
